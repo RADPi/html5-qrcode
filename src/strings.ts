685d53b0e744d690241658eb0378cb0a41cd6e39
Rafael Dávila Pico <59027415+RADPi@users.noreply.github.com>
2025-05-27 18:08:24 -0300

    Agregar traducciones al español para mensajes de error y estado en Html5Qrcode y Html5QrcodeScanner

--- src/strings.ts
+++ src/strings.ts
@@ -1,200 +1,188 @@
 /**
  * @fileoverview
  * Strings used by {@class Html5Qrcode} & {@class Html5QrcodeScanner}
- * 
+ *
  * @author mebjas <minhazav@gmail.com>
- * 
+ *
  * The word "QR Code" is registered trademark of DENSO WAVE INCORPORATED
  * http://www.denso-wave.com/qrcode/faqpatent-e.html
  */
 
 /**
  * Strings used in {@class Html5Qrcode}.
- * 
+ *
  * TODO(mebjas): Support internalization.
  */
 export class Html5QrcodeStrings {
+	public static codeParseError(exception: any): string {
+		return `QR code parse error, error = ${exception}`
+	}
 
-    public static codeParseError(exception: any): string {
-        return `QR code parse error, error = ${exception}`;
-    }
+	public static errorGettingUserMedia(error: any): string {
+		return `Error getting userMedia, error = ${error}`
+	}
 
-    public static errorGettingUserMedia(error: any): string {
-        return `Error getting userMedia, error = ${error}`;
-    }
+	public static onlyDeviceSupportedError(): string {
+		return (
+			"The device doesn't support navigator.mediaDevices , only " +
+			'supported cameraIdOrConfig in this case is deviceId parameter ' +
+			'(string).'
+		)
+	}
 
-    public static onlyDeviceSupportedError(): string {
-        return "The device doesn't support navigator.mediaDevices , only "
-        + "supported cameraIdOrConfig in this case is deviceId parameter "
-        + "(string).";
-    }
+	public static cameraStreamingNotSupported(): string {
+		return 'Camera streaming not supported by the browser.'
+	}
 
-    public static cameraStreamingNotSupported(): string {
-        return "Camera streaming not supported by the browser.";
-    }
+	public static unableToQuerySupportedDevices(): string {
+		return 'Unable to query supported devices, unknown error.'
+	}
 
-    public static unableToQuerySupportedDevices(): string {
-        return "Unable to query supported devices, unknown error.";
-    }
+	public static insecureContextCameraQueryError(): string {
+		return (
+			'Camera access is only supported in secure context like https ' +
+			'or localhost.'
+		)
+	}
 
-    public static insecureContextCameraQueryError(): string {
-        return "Camera access is only supported in secure context like https "
-        + "or localhost.";
-    }
-
-    public static scannerPaused(): string {
-        return "Scanner paused";
-    }
+	public static scannerPaused(): string {
+		return 'Scanner paused'
+	}
 }
 
 /**
  * Strings used in {@class Html5QrcodeScanner}.
- * 
+ *
  * TODO(mebjas): Support internalization.
  */
 export class Html5QrcodeScannerStrings {
+	public static scanningStatus(): string {
+		return 'Escaneando'
+	}
 
-    public static scanningStatus(): string {
-        return "Scanning";
-    }
+	public static idleStatus(): string {
+		return 'Inactivo'
+	}
 
-    public static idleStatus(): string {
-        return "Idle";
-    }
+	public static errorStatus(): string {
+		return 'Error'
+	}
 
-    public static errorStatus(): string {
-        return "Error";
-    }
+	public static permissionStatus(): string {
+		return 'Permiso'
+	}
 
-    public static permissionStatus(): string {
-        return "Permission";
-    }
+	public static noCameraFoundErrorStatus(): string {
+		return 'Sin cámaras'
+	}
 
-    public static noCameraFoundErrorStatus(): string {
-        return "No Cameras";
-    }
+	public static lastMatch(decodedText: string): string {
+		return `Última coincidencia: ${decodedText}`
+	}
 
-    public static lastMatch(decodedText: string): string {
-        return `Last Match: ${decodedText}`;
-    }
+	public static codeScannerTitle(): string {
+		return 'Escáner de códigos'
+	}
 
-    public static codeScannerTitle(): string {
-        return "Code Scanner";
-    }
+	public static cameraPermissionTitle(): string {
+		return 'Solicitar permisos de cámara'
+	}
 
-    public static cameraPermissionTitle(): string {
-        return "Request Camera Permissions";
-    }
+	public static cameraPermissionRequesting(): string {
+		return 'Solicitando permisos de cámara...'
+	}
 
-    public static cameraPermissionRequesting(): string {
-        return "Requesting camera permissions...";
-    }
+	public static noCameraFound(): string {
+		return 'No se encontró cámara'
+	}
 
-    public static noCameraFound(): string {
-        return "No camera found";
-    }
+	public static scanButtonStopScanningText(): string {
+		return 'Detener escaneo'
+	}
 
-    public static scanButtonStopScanningText(): string {
-        return "Stop Scanning";
-    }
+	public static scanButtonStartScanningText(): string {
+		return 'Iniciar escaneo'
+	}
 
-    public static scanButtonStartScanningText(): string {
-        return "Start Scanning";
-    }
+	public static torchOnButton(): string {
+		return 'Encender linterna'
+	}
 
-    public static torchOnButton(): string {
-        return "Switch On Torch";
-    }
+	public static torchOffButton(): string {
+		return 'Apagar linterna'
+	}
 
-    public static torchOffButton(): string {
-        return "Switch Off Torch";
-    }
+	public static torchOnFailedMessage(): string {
+		return 'No se pudo encender la linterna'
+	}
 
-    public static torchOnFailedMessage(): string {
-        return "Failed to turn on torch";
-    }
+	public static torchOffFailedMessage(): string {
+		return 'No se pudo apagar la linterna'
+	}
 
-    public static torchOffFailedMessage(): string {
-        return "Failed to turn off torch";
-    }
+	public static scanButtonScanningStarting(): string {
+		return 'Iniciando cámara...'
+	}
 
-    public static scanButtonScanningStarting(): string {
-        return "Launching Camera...";
-    }
+	public static textIfCameraScanSelected(): string {
+		return 'Escanear un archivo de imagen'
+	}
 
-    /**
-     * Text to show when camera scan is selected.
-     * 
-     * This will be used to switch to file based scanning.
-     */
-    public static textIfCameraScanSelected(): string {
-        return "Scan an Image File";
-    }
+	public static textIfFileScanSelected(): string {
+		return 'Escanear usando la cámara directamente'
+	}
 
-    /**
-     * Text to show when file based scan is selected.
-     * 
-     * This will be used to switch to camera based scanning.
-     */
-    public static textIfFileScanSelected(): string {
-        return "Scan using camera directly";
-    }
+	public static selectCamera(): string {
+		return 'Seleccionar cámara'
+	}
 
-    public static selectCamera(): string {
-        return "Select Camera";
-    }
+	public static fileSelectionChooseImage(): string {
+		return 'Elegir imagen'
+	}
 
-    public static fileSelectionChooseImage(): string {
-        return "Choose Image";
-    }
+	public static fileSelectionChooseAnother(): string {
+		return 'Elegir otra'
+	}
 
-    public static fileSelectionChooseAnother(): string {
-        return "Choose Another";
-    }
+	public static fileSelectionNoImageSelected(): string {
+		return 'Ninguna imagen seleccionada'
+	}
 
-    public static fileSelectionNoImageSelected(): string {
-        return "No image choosen";
-    }
+	public static anonymousCameraPrefix(): string {
+		return 'Cámara anónima'
+	}
 
-    /** Prefix to be given to anonymous cameras. */
-    public static anonymousCameraPrefix(): string {
-        return "Anonymous Camera";
-    }
+	public static dragAndDropMessage(): string {
+		return 'O arrastra una imagen para escanear'
+	}
 
-    public static dragAndDropMessage(): string {
-        return "Or drop an image to scan";
-    }
+	public static dragAndDropMessageOnlyImages(): string {
+		return 'O arrastra una imagen para escanear (otros archivos no soportados)'
+	}
 
-    public static dragAndDropMessageOnlyImages(): string {
-        return "Or drop an image to scan (other files not supported)";
-    }
+	public static zoom(): string {
+		return 'zoom'
+	}
 
-    /** Value for zoom. */
-    public static zoom(): string {
-        return "zoom";
-    }
+	public static loadingImage(): string {
+		return 'Cargando imagen...'
+	}
 
-    public static loadingImage(): string {
-        return "Loading image...";
-    }
+	public static cameraScanAltText(): string {
+		return 'Escaneo con cámara'
+	}
 
-    public static cameraScanAltText(): string {
-        return "Camera based scan";
-    }
-
-    public static fileScanAltText(): string {
-        return "Fule based scan";
-    }
+	public static fileScanAltText(): string {
+		return 'Escaneo desde archivo'
+	}
 }
 
-/** Strings used in {@class LibraryInfoDiv} */
 export class LibraryInfoStrings {
+	public static poweredBy(): string {
+		return 'Desarrollado por '
+	}
 
-    public static poweredBy(): string {
-        return "Powered by ";
-    }
-
-    public static reportIssues(): string {
-        return "Report issues";
-    }
+	public static reportIssues(): string {
+		return 'Reportar problemas'
+	}
 }
